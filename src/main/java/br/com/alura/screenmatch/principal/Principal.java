8aa675c8ef07b52d3191cbab35c0f435896dc892
cleber-campos <cleber.campos06@gmail.com>
2024-08-07 00:30:54 -0300

    Merge 07.08

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -33,9 +33,17 @@ public class Principal {
 
               case 2:
                   buscarSeriePorTitulo(validaEntradaSerie());                   //3. Buscar Serie por Titulo
-                  break;
+              break;
 
               case 3:
+                  buscarSeriePorAtor();                                        //3. Buscar Serie por Ator
+               break;
+
+              case 4:
+                  buscarSerieTop5();
+              break;
+
+              case 5:
                   headerSerie();
                   List<Series> listaSeries = repository.findAll();
                   listaSeries.stream()
@@ -44,10 +52,10 @@ public class Principal {
                           .forEach(System.out::println);                        //4. Listar series buscadas
                   break;
 
-              case 4:
-                  new BuscarEpisodio()
-                          .getData(repository, validaEntradaSerie());           //5. Buscar episodios da series
-                  break;
+                case 6:
+                    new BuscarEpisodio()
+                            .getData(repository, validaEntradaSerie());           //5. Buscar episodios da series
+                    break;
 
               case 0:
                   System.out.println("Saindo....");                             //6. Sair
@@ -60,6 +68,15 @@ public class Principal {
         }
     }
 
+    private void buscarSerieTop5() {
+    var seriesTop5 = repository
+            .findTop5ByOrderByAvaliacaoDesc();
+    if(!seriesTop5.isEmpty()) {
+        headerSerie();
+        seriesTop5.forEach(s -> System.out.println("Avaliacao: " + s.getAvaliacao() + " - "+ s.getTituloSerie()));
+    } else System.out.println("Serie nao encontrada");
+}
+
     private void buscarSeriePorTitulo(String nomeSerie) {
         Optional<Series> serieBuscada = repository
                 .findByTituloSerieContainingIgnoreCase(nomeSerie);
@@ -69,6 +86,21 @@ public class Principal {
         } else {
             System.out.println("Serie nao identificada!");
         }
+    }
+
+    private void buscarSeriePorAtor() {
+        System.out.print("Qual o nome do ator: ");
+        var nomeAtor = leitura.nextLine();
+
+        System.out.print("Qual o valor da avaliacao min: ");
+        var avaliacao = leitura.nextDouble();
+
+        if (!nomeAtor.isEmpty()) {
+            var seriesEncontradas = repository
+                    .findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+            headerSerie();
+            seriesEncontradas.forEach(s -> System.out.println(s.getTituloSerie() + ", avaliacao: " + s.getAvaliacao()));
+        } else System.out.println("Dados nao encontrado!");
     }
 
     public String validaEntradaSerie() {
@@ -93,8 +125,10 @@ public class Principal {
                   ------------------------------
                   1 - Buscar Serie na Web
                   2 - Buscar Serie por Titulo
-                  3 - Listar Todas as Series
-                  4 - Buscar Epsodios
+                  3 - Buscar Serie por Ator
+                  4 - Buscar Top 5 Series
+                  5 - Listar Todas as Series
+                  6 - Buscar Epsodios
                   0 - Sair
                   ------------------------------
                   """;
--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -3,9 +3,13 @@ package br.com.alura.screenmatch.repository;
 import br.com.alura.screenmatch.model.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Series, Long> {
     Optional<Series> findByTituloSerieContainingIgnoreCase(String nomeSerie);
 
+    List<Series> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
+
+    List<Series> findTop5ByOrderByAvaliacaoDesc();
 }
